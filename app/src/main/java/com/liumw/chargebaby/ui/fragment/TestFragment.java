be3be5b729dc029104c65e3ed731aadff4e1e60a
eudemon000 <smile0506>
2016-07-29 17:35:13 +0800

    高德地图

--- app/src/main/java/com/liumw/chargebaby/ui/fragment/TestFragment.java
+++ app/src/main/java/com/liumw/chargebaby/ui/fragment/TestFragment.java
@@ -1,30 +1,142 @@
 package com.liumw.chargebaby.ui.fragment;
 
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.amap.api.location.AMapLocation;
+
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.LocationSource;
+import com.amap.api.maps.MapView;
 import com.liumw.chargebaby.R;
+
+import org.xutils.view.annotation.ContentView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class TestFragment extends Fragment {
+@ContentView(R.layout.fragment_test)
+public class TestFragment extends Fragment implements LocationSource, AMapLocationListener {
 
+    private MapView mapView;
 
-    public TestFragment() {
-        // Required empty public constructor
-    }
+    private AMap aMap;
+
+    private AMapLocationClientOption mLocationOption = null;
+
+    private AMapLocationClient mLocationClient;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_test, container, false);
+        View view = inflater.inflate(R.layout.fragment_test, container, false);
+        mapView = (MapView) view.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        return view;
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        init(getActivity());
+    }
+
+    private void init(Context context) {
+        aMap = mapView.getMap();
+        aMap.setLocationSource(this);
+        aMap.setMyLocationEnabled(true);
+        aMap.getUiSettings().setMyLocationButtonEnabled(true);
+        aMap.setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
+        mLocationClient = new AMapLocationClient(context);
+        mLocationClient.setLocationListener(this);
+        mLocationOption = new AMapLocationClientOption();
+        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        mLocationClient.setLocationOption(mLocationOption);
+        mLocationClient.startLocation();
+    }
+
+    @Override
+    public void activate(OnLocationChangedListener onLocationChangedListener) {
+        int i = 0;
+    }
+
+    @Override
+    public void deactivate() {
+        int i = 0;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLocationChanged(AMapLocation aMapLocation) {
+        int i = 0;
+        Log.i("tag", aMapLocation.getErrorInfo());
+        Log.i("tag", aMapLocation.getErrorCode() + "");
+        /*if (aMapLocation != null) {
+            if (aMapLocation.getErrorCode() == 0) {
+                //定位成功回调信息，设置相关消息
+                aMapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                aMapLocation.getLatitude();//获取纬度
+                aMapLocation.getLongitude();//获取经度
+                aMapLocation.getAccuracy();//获取精度信息
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                Date date = new Date(aMapLocation.getTime());
+                df.format(date);//定位时间
+            } else {
+                //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                Log.e("AmapError","location Error, ErrCode:"
+                        + aMapLocation.getErrorCode() + ", errInfo:"
+                        + aMapLocation.getErrorInfo());
+            }
+        }*/
+        if (aMapLocation != null) {
+            if (aMapLocation != null
+                    && aMapLocation.getErrorCode() == 0) {
+                //mLocationErrText.setVisibility(View.GONE);
+                //mListener.onLocationChanged(aMapLocation);// 显示系统小蓝点
+            } else {
+                /*String errText = "定位失败," + amapLocation.getErrorCode()+ ": " + amapLocation.getErrorInfo();
+                Log.e("AmapErr",errText);
+                mLocationErrText.setVisibility(View.VISIBLE);
+                mLocationErrText.setText(errText);*/
+            }
+        }
+    }
 }
