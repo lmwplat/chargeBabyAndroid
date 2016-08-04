ec9f0f3b78b12e5f344093018e5b23ee1baf5367
liumw1203 <liumw1203@aliyun.com>
2016-08-04 10:33:53 +0800

    更新高德

---
+++ app/src/main/java/com/liumw/chargebaby/download/DownloadAsyncTask.java
@@ -0,0 +1,7 @@
+package com.liumw.chargebaby.download;
+
+/**
+ * Created by liumw on 2016/8/4 0004.
+ */
+public class DownloadAsyncTask {
+}
--- app/src/main/java/com/liumw/chargebaby/ui/SearchActivity.java
+++ app/src/main/java/com/liumw/chargebaby/ui/SearchActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,10 +15,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.liumw.chargebaby.R;
+import com.liumw.chargebaby.db.DBManager;
+import com.liumw.chargebaby.entity.Charge;
 
+import org.xutils.DbManager;
+import org.xutils.ex.DbException;
 import org.xutils.view.annotation.ContentView;
 import org.xutils.view.annotation.ViewInject;
 import org.xutils.x;
+
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -29,6 +36,9 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
     private TextView tvSearch;
     @ViewInject(R.id.recyclerView)
     private RecyclerView recyclerView;
+    //数据库处理
+    DbManager.DaoConfig daoConfig= DBManager.getDaoConfig();
+    DbManager db = x.getDb(daoConfig);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +48,13 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new SearchAdapter());
         ivBack.setOnClickListener(this);
+
+        try {
+            List<Charge> all = db.selector(Charge.class).where("area","like","%天%").or("name","like","%酒店%").findAll();
+            Log.e("db", all.toString());
+        } catch (DbException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -74,6 +91,9 @@ public class SearchActivity extends AppCompatActivity implements View.OnClickLis
                 super(itemView);
                 t = (TextView) itemView.findViewById(R.id.tv_title);
             }
+
+
+
         }
     }
 
